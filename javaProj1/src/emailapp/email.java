f51d3d014e39be71b380d203d229711795a01070
HARINI S <167424534+Harini-S109@users.noreply.github.com>
2024-07-08 22:50:25 +0530

    Update email.java

--- javaProj1/src/emailapp/email.java
+++ javaProj1/src/emailapp/email.java
@@ -19,8 +19,10 @@ public class email {
 		this.department = setDepartment();
 		System.out.println("Your department: "+ this.department);
 		
-		this.randEmail = setEmail();
-		System.out.println("Your random generated: "+ this.randEmail);
+		this.randEmail = setREmail();
+		System.out.println("Your random generated email: "+ this.randEmail);
+		
+		chooseOpt();
 	}
 	
 	//ask name
@@ -51,11 +53,40 @@ public class email {
 		}
 	}
 	
-	// set email
-	public String setEmail() {
+	// set random email
+	public String setREmail() {
 		int rand = (int) (Math.random() * 900);
-		String em = this.firstName.toLowerCase() + this.lastName.toLowerCase() + this.department.toLowerCase() + rand + "@mail.com";
+		String em = this.firstName.toLowerCase() + this.lastName.toLowerCase() + this.department.toLowerCase() + rand + "@company.com";
 		return em;
+	}
+	
+	// set email
+	public void setEmail() {
+		System.out.println("Enter your desired name for email: ");
+		String eml = in.nextLine();
+		this.randEmail = eml.toLowerCase()+this.department.toLowerCase()+"@company.com";
+	}
+	
+	//display data
+	public void dispData() {
+		System.out.println("Name: "+ this.firstName + " " + this.lastName);
+		System.out.println("Department: "+ this.department);
+		System.out.println("Email: "+ this.randEmail);
+	}
+	
+	
+	public void chooseOpt() {
+		System.out.println("Choose: \n1 Change email \n2 Display data");
+		int opt = in.nextInt();
+		if(opt == 1) {
+			setEmail();
+		}
+		else if(opt == 2) {
+			dispData();
+		}
+		else {
+			System.out.println("Enter a valid option.");
+		}
 	}
 }
 
